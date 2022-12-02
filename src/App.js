1897b656280cf86cf82ea68e23e3c36b2ef7cad1
Odewole Saheed Adesina <odewolesaheed@gmail.com>
2022-12-02 09:38:22 +0100

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Nav from './main_nav';
+import Home from './Home';
+import About from './About'
+import Contact from './Contact';
+import Career from './Career'
+import NewStudent from './NewStudent';
+import ReturnStudent from './ReturnStudent'
+import Signup from './Signup';
+import Login from './Login'
+import PageFooter from './PageFooter'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container-fluid">
+        <Nav />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/career" element={<Career />} />
+          <Route path="/new-student" element={<NewStudent />} />
+          <Route path="/returning-student" element={<ReturnStudent />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <PageFooter />
+      </div>
+    </Router>
   );
 }
 
